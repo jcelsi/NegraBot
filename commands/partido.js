edd78f23e6f23352c98bb9fca2ccb489dd214561
jcelsi <43588844+jcelsi@users.noreply.github.com>
2022-12-01 15:30:58 -0300

    first commit

---
+++ commands/partido.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('partido-mundial')
+		.setDescription('Devuelve fechas de partidos')
+		.addStringOption(option =>
+			option.setName('fecha')
+				.setDescription('Dia de los partidos.')),
+	async execute(interaction) {
+		const current_date = new Date();
+		const year = current_date.getFullYear();
+		const month = current_date.getMonth() + 1;
+		const day = current_date.getDate();
+		let dateWithHyphens;
+		let reply = '';
+
+		switch (interaction.options.getString('fecha')) {
+		case 'hoy':
+			reply = 'Los partidos para hoy son:\n';
+			dateWithHyphens = [year, month, '0' + day].join('-');
+			break;
+		case null:
+			reply = 'Los partidos para hoy son:\n';
+			dateWithHyphens = [year, month, '0' + day].join('-');
+			break;
+		case 'mañana':
+			reply = 'Los partidos para mañana son:\n';
+			dateWithHyphens = [year, month, '0' + (day + 1)].join('-');
+			break;
+		default:
+			const userDate = interaction.options.getString('fecha').split(' ');
+			reply = `Los partidos para el ${userDate[0]} ${userDate[1]} son:\n`;
+			dateWithHyphens = [year, month, '0' + userDate[1]].join('-');
+			break;
+		}
+
+		const config = {
+			headers: { 'X-Auth-Token': '8ec7de5a5fb84f31ba8ae4f408a8082e', 'Accept-Encoding': 'application/json' },
+		};
+		axios.get(`https://api.football-data.org/v4/competitions/WC/matches?dateFrom=${dateWithHyphens}&dateTo=${dateWithHyphens}`, config)
+			.then((response) => {
+				for (const match of response.data.matches) {
+					const homeTeam = match.homeTeam.name;
+					const awayTeam = match.awayTeam.name;
+					if (match.status == 'FINISHED') {
+						const teams = `${homeTeam} vs ${awayTeam}`;
+						reply += teams + `   (${match.score.fullTime.home}-${match.score.fullTime.away})\n`;
+					}
+					else {
+						const teams = `${homeTeam} vs ${awayTeam}`;
+						reply += teams + '\n';
+					}
+				}
+				return interaction.reply(reply);
+			});
+	},
+};
