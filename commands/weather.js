0ed125612be3ac06ef6f317e9cb35271050605fd
jcelsi <43588844+jcelsi@users.noreply.github.com>
2022-12-02 15:42:56 -0300

    finished weather command

--- commands/weather.js
+++ commands/weather.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,6 +10,31 @@ module.exports = {
 				.setDescription('Ciudad')
 				.setRequired(true)),
 	async execute(interaction) {
-		return interaction.reply('Clima!');
+
+		let city = interaction.options.getString('city');
+		city = city.charAt(0).toUpperCase() + city.slice(1);
+		city = city == 'Santiago' ? 'Santiago, Chile' : city;
+
+		await interaction.deferReply();
+
+		const config = {
+			headers: { 'Accept': '*/*', 'User-Agent': 'curl/7.43.0', 'Accept-Language': 'es-ES' },
+		};
+
+		await axios.get(`http://wttr.in/${city}?m`, config)
+			.then((response) => {
+				const raw = response.data.split('\n');
+				const idx = raw.findIndex(el => /\s+┌─────────────┐\s+/.test(el));
+				const result = raw
+					.slice(0, idx)
+					.map(text => text.replace(/\[(\d+)?((;\d+)+)?(m)?/g, ''))
+					.join('\n')
+					.replaceAll('\x1B', '');
+
+				return interaction.editReply('```ansi\n' + result + '\n```');
+			})
+			.catch(error => {
+				return interaction.editReply('```ansi\n Ciudad no encontrada ```');
+			});
 	},
 };
