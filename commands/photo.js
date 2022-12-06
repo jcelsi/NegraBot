104129864ed0b82d9a9c4136464ce010a2052ada
jcelsi <43588844+jcelsi@users.noreply.github.com>
2022-12-05 21:20:18 -0300

    added negra photos

--- commands/photo.js
+++ commands/photo.js
@@ -1,31 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
+const data = require('../data/negra.json');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('photo')
 		.setDescription('Foto random de la negra'),
 	async execute(interaction) {
 
+		console.log(data);
 		const exampleEmbed = new EmbedBuilder()
 			.setColor(0x0099FF)
-			.setTitle('Some title')
-			.setURL('https://discord.js.org/')
-			.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+			.setTitle('Negra')
 			.setDescription('Some description here')
-			.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-			.addFields(
-				{ name: 'Regular field title', value: 'Some value here' },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-			)
-			.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-			.setImage('https://i.imgur.com/AfFp7pu.png')
-			.setTimestamp()
-			.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-		//channel.send({ embeds: [exampleEmbed] }); 
+			.setImage(data.urls.sample());
 
 		return interaction.reply({ embeds: [exampleEmbed] });
 	},
